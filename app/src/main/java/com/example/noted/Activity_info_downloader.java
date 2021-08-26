cf6ac7097a509a037593b13cd9b06ddf9cef014f
Sidorov Stanislav Dm <sidorovstasd7@gmail.com>
2021-08-26 17:29:57 +0300

    info downloader

--- app/src/main/java/com/example/noted/Activity_info_downloader.java
+++ app/src/main/java/com/example/noted/Activity_info_downloader.java
@@ -1,13 +1,17 @@
 package com.example.noted;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -17,11 +21,35 @@ public class Activity_info_downloader extends AppCompatActivity {
     FirebaseDatabase db;
     DatabaseReference users;
 
-    public static ArrayList<String> штащ;
+    public static ArrayList<String> user_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_downloader);
+
+        auth=FirebaseAuth.getInstance();
+        db=FirebaseDatabase.getInstance();
+        users=db.getReference().child(Activity_a_login_registration.Username).child(Activity_group_table.chosen_group).child("Names");
+
+        user_info=new ArrayList<>();
+
+        users.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot snapshot) {
+
+                    for (DataSnapshot data : snapshot.getChildren()) {
+                        System.out.println(data.getKey().toString());
+                        user_info.add(data.getKey().toString());
+                        startActivity(new Intent(Activity_info_downloader.this, Activity_people_table.class));
+                    }
+                }
+            @Override
+            public void onCancelled( DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
