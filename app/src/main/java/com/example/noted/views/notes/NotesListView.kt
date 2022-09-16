8e46e76e62cab5636f1a0d1fc055f9ea49e69f1f
MShizik <sidorovstasd7@gmail.com>
2022-09-16 10:54:47 +0300

    uuuuu

--- app/src/main/java/com/example/noted/views/notes/NotesListView.kt
+++ app/src/main/java/com/example/noted/views/notes/NotesListView.kt
@@ -13,9 +13,26 @@ class NotesListView(var rootView : View) {
     var lvNotesList : ListView = rootView.findViewById(R.id.notes_lv_info)
 
     var pbNotesLoader : ProgressBar = rootView.findViewById(R.id.notes_progress_bar)
+
+
     fun setDirectoryWay( way : String){
         tvDirectory.text = way
     }
 
+    fun setStartPosition(){
+        lvNotesList.visibility = View.VISIBLE
+        lvNotesList.alpha = 0F
+        pbNotesLoader.visibility = View.VISIBLE
+        pbNotesLoader.translationX = ((rootView.width+100) * -1).toFloat()
+    }
+
+    fun startLoadingAnimation(){
+        pbNotesLoader.animate().setDuration(500L).translationX(0F).setListener(null)
+    }
+
+    fun endLoadingAnimation(){
+        pbNotesLoader.animate().setDuration(500L).translationX((rootView.width+100).toFloat()).withEndAction{pbNotesLoader.visibility = View.GONE}
+        lvNotesList.animate().setDuration(500L).alpha(1.0F).setListener(null)
+    }
 
 }
